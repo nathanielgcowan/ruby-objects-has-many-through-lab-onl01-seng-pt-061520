5064e56eeeddd08efa461118fed3ffe8454aa7ac
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-26 01:57:45 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all =[]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    appointment.new(date, patient, self)
+  end
+  
+  def appointments
+    appointment.all.select { |appointment| appointment.
