682f4aa9fb73af3492b2c8eae9cb81bb63c8eebe
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-26 01:03:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,26 @@
 class Artist
+  attr_acessor :name
   
   @@all =[]
   
   def initialize(name)
     @name = name
     @@all << self
-    @songs = song
-    @genres = genres
   end
   
-  def artist.all
-    
+  def self.all
+    @all
   end
   
   def new_song(name, genre)
-    song.new
+    song.new(name, self, genre)
+  end
+  
+  def songs
+    song.all.select { |song| song.artist == self|
+  end
+  
+  def genres
+    songs.map(&:genre)
   end
 end
